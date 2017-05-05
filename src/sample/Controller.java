41c011f6c0df4ceb2be488ed15b54746a5686909
ankaim <kaim@mail.ru>
2017-05-05 15:10:37 +0200

    Добавлен поиск по id

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,27 +11,34 @@ public class Controller {
     public TextField textField;
     public TextArea textArea;
     static String tstat;
+    static int id;
     public void pogoda() throws IOException {
         tstat = textField.getText();
         textField.clear();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("1.txt"), "UTF-8"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("listStad.txt"), "UTF-8"));
         String[] temp;
         String sd;
         int stad = 0;
         textArea.clear();
-        textArea.appendText("Вы ввели что то не то!)))\n Введите начало слова, выберите подходящий вариант\n И введите еще раз правильно!\n К примеру: (моск), если ищите Москву\n\n");
+        textArea.appendText("Вы ввели что то не то!)))\n Введите начало слова, выберите подходящий вариант\n И введите еще раз правильно!\n Ввести можно название города или id\n К примеру: (моск), если ищите Москву\n\n");
         while (reader.ready()){
             if(tstat!=" ") {
-                if ((sd = reader.readLine().toLowerCase()).contains(tstat.toLowerCase())) {
-                    temp = sd.split(" ");
-                    if (temp[1].equals(tstat.toLowerCase())) {
+                if ((sd = reader.readLine().toLowerCase()).contains(tstat.toLowerCase())) {//Проверяем есть в строке похожее
+                    temp = sd.split(" : ");
+                    if (temp[1].equals(tstat.toLowerCase())) {//Проверяем наличие точного совпадения
                         stad = Integer.parseInt(temp[0]);
                         break;
                     } else {
                         if (temp[1].startsWith(tstat.toLowerCase())) {
                             textField.clear();
-                            textArea.appendText(sd.substring(sd.indexOf(" ")) + "\n");
+                            textArea.appendText("id "+temp[0]+sd.substring(sd.indexOf(" ")) + "\n");//Выводит варианты пожие на введенный
                         }
+                    }
+                    try {
+                        id=Integer.parseInt(tstat);//Вывод города по id
+                        stad=id;
+                    }catch (Exception e){
+                        break;
                     }
                 }
             }
